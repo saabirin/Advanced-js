78081f9ddf45899ccdb2eaff077925cd51606f54
Queen Saaba <sabirinali56@gmail.com>
2022-02-28 12:38:01 -0800

    file added

---
+++ Main.js
@@ -0,0 +1,30 @@
+// /Create a CLass Student
+ class Student {
+    id="C119007"
+    name ="Sabirin Ali";
+    age=20;
+    getData(
+    ){
+
+        return `ID is : ${this.id} Name is : ${this.name}
+         Age is : ${this.age}`
+
+    }
+
+}
+//Student Object
+const std =new Student()
+console.log(std.getData())
+
+///Class Rectangle
+const Rectangle=class{
+    constructor(height,width)
+    {
+        this.height=height
+        this.width=width;
+    }
+    area(){
+        return `Area is :${this.height *this.width}`
+    }
+}
+console.log(new Rectangle(12,7).area());
